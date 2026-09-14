17c133c427572f475aabc59a53c93cfff9f21d6c
Stanisław <117434202+Anaxar1@users.noreply.github.com>
2026-09-14 15:01:58 +0200

    Bot behavior
    Bot being spawned at random location

---
+++ Bot.java
@@ -0,0 +1,59 @@
+import greenfoot.Actor;
+import greenfoot.Color;
+import greenfoot.GreenfootImage;
+
+
+/**
+ * A paddle is an object that goes back and forth. Though it would be nice if balls would bounce of it.
+ *
+ * @author The teachers
+ * @version 1
+ */
+public class Bot extends Actor {
+    private int width;
+    private int height;
+    private int dx;
+
+    /**
+     * Constructs a new paddle with the given dimensions.
+     */
+    public Bot(int width, int height) {
+        this.width = width;
+        this.height = height;
+        dx = 1;
+        createImage();
+    }
+
+    /**
+     * Act - do whatever the Bot wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() {
+        tryChangeDirection();
+        setLocation(getX() + dx, getY());
+        createNewBot();    
+    }
+
+    private void createNewBot(){
+        if (isAtEdge()){
+            getWorld().removeObject(this);    
+        }
+    }
+   
+    
+    /**
+     * Will rotate the paddle 180 degrees if the paddle is at worlds edge.
+     */
+   
+
+    /**
+     * Creates and sets an image for the paddle, the image will have the same dimensions as the paddles width and height.
+     */
+    private void createImage() {
+        GreenfootImage image = new GreenfootImage(width, height);
+        image.setColor(Color.BLACK);
+        image.fill();
+        setImage(image);
+    }
+
+}
