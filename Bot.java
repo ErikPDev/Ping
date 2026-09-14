d3022ac9f42976c561a70a9dc867767cc4b28278
Erik <33035080+ErikPDev@users.noreply.github.com>
2026-09-14 15:43:33 +0200

    Refactor Bot and Player to extend Paddle
    Move shared paddle setup into `Paddle` and make `Bot`/`Player` inherit it via `super(width, height)`, removing duplicated width/height fields and image creation logic. `Paddle` now keeps dimensions immutable, and its default movement `act()` is disabled so subclasses control behavior explicitly.

--- Bot.java
+++ Bot.java
@@ -9,19 +9,15 @@ import greenfoot.Greenfoot;
  * @author The teachers
  * @version 1
  */
-public class Bot extends Actor {
-    private int width;
-    private int height;
-    private int dx;
+public class Bot extends Paddle {
+
+    private int dx = 1;
 
     /**
      * Constructs a new paddle with the given dimensions.
      */
     public Bot(int width, int height) {
-        this.width = width;
-        this.height = height;
-        dx = 1;
-        createImage();
+        super(width, height);
     }
 
     /**
@@ -44,22 +40,6 @@ public class Bot extends Actor {
         
         getWorld().addObject(bot, 0, botY);      
         getWorld().removeObject(this);       
-    }
-   
-    
-    /**
-     * Will rotate the paddle 180 degrees if the paddle is at worlds edge.
-     */
-   
-
-    /**
-     * Creates and sets an image for the paddle, the image will have the same dimensions as the paddles width and height.
-     */
-    private void createImage() {
-        GreenfootImage image = new GreenfootImage(width, height);
-        image.setColor(Color.BLACK);
-        image.fill();
-        setImage(image);
     }
 
 }
