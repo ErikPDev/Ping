32d290517c36b13773e1b58805a69b6d1b57d8dc
kasumjosip <kasumjosip@gmail.com>
2026-09-15 20:02:39 +0200

    Make into screen

--- IntroWorld.java
+++ IntroWorld.java
@@ -1,4 +1,5 @@
 import greenfoot.Color;
+import greenfoot.Font;
 import greenfoot.Greenfoot;
 import greenfoot.GreenfootImage;
 import greenfoot.World;
@@ -18,9 +19,23 @@ public class IntroWorld extends World {
      */
     public IntroWorld() {
         super(WORLD_WIDTH, WORLD_HEIGHT, 1);
+        setBackground("space.jpg");
         GreenfootImage background = getBackground();
-        background.setColor(Color.BLACK);
-        background.drawString("Intro world. Hit <enter> to start game...", WORLD_WIDTH / 2 - 100, WORLD_HEIGHT / 2);
+        Font gameFont = new Font("Courier New", true, false, 50);
+        background.setFont(gameFont);
+        background.setColor(Color.GREEN);
+        background.drawString("THE PING GAME", WORLD_WIDTH / 2 - 200, WORLD_HEIGHT / 6 );
+        Font gameFont1 = new Font("Courier New", true, false, 15);
+        background.setFont(gameFont1);
+        background.drawString("Made by : Space Pong",  10, 690 );
+        Font gameFont2 = new Font("Courier New", true, false, 20);
+        background.setFont(gameFont2);
+        background.setColor(Color.GREEN);
+        background.drawString("Press ENTER to start", 130 , 350 );
+        Font gameFont3 = new Font("Courier New", true, false, 15);
+        background.setFont(gameFont3);
+        background.setColor(Color.GREEN);
+        background.drawString("Use left and right => keys to move!", 95 , 450 );
     }
 
     public void act() {
